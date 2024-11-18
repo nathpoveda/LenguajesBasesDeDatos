96b3a8cf47b98329e38eca028aa957e23f693c0c
Nathalie Poveda Chaves <nathalie.pch09@gmail.com>
2024-11-18 11:44:47 -0600

    Se agrego el proyecto de netbeans

---
+++ Proyecto_InnovaTech/src/test/java/com/innovatech/InnovatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
